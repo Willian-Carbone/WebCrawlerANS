90811fefb31e964d40f4ba88df21029e499ea323
Willian Carbone Bueno <williancarbonebueno@gmail.com>
2026-03-29 13:08:07 -0300

    feat[base]: base do projeto

---
+++ src/main/groovy/utilitarios/Utilitarios.groovy
@@ -0,0 +1,9 @@
+package utilitarios
+
+class Utilitarios {
+
+    static boolean validadorEmail (String email){
+        return (email ==~ /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/)
+    }
+
+}
